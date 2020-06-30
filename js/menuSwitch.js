01280f011d1b18114b11ea50cc363d3b21279d04
yxf <599367320@qq.com>
2020-06-30 19:15:14 +0800

    修改

--- js/menuSwitch.js
+++ js/menuSwitch.js
@@ -4,23 +4,13 @@ var menuUl = document.querySelector(".header .menu");
 var menusLi = document.querySelectorAll(".header .sub-nav .menus li");
 
 //绑定事件 - 鼠标移入
-menuUl.onmouseover = function(e) {
-    e.stopPropagation(); //取消事件冒泡
-    if (e.target.tagName != "A") {
-        console.log(2);
-        return;
-    }
-
-    console.log(1);
+menuUl.addEventListener('mouseover', function(e) {
     //清除之前的active
     var before = document.querySelector(".header .menu .active");
     // console.log(before)
     before.className = "";
-}
+});
 
-menusLi.forEach(function(ele, index) {
-    ele.onmouseout = function() {
-        console.log(menuUl.children[1])
-        menuUl.children[1].className = "active";
-    }
-})
+menuUl.addEventListener('mouseout', function(e) {
+    menuUl.children[0].className = "active";
+});
